2007c981adf61a44725dd4dbefa0b31eede3035c
Jefferson Estevam <jesbmail@gmail.com>
2020-03-19 13:47:55 -0300

    add react modal to show movies info

--- src/components/elements/contentList/index.jsx
+++ src/components/elements/contentList/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col } from 'react-grid-system';
 import './styles.scss';
 import ContentModal from '../contentModal';
@@ -7,15 +7,29 @@ import ContentModal from '../contentModal';
 
 const ContentList = () => {
 
-    function handleModal(e) {
-        console.log(e)
+    const [isOpen, setIsOpen] = useState(false)
+
+    function handleModal() {
+        setIsOpen(!isOpen);
     }
 
     return (
         <div className="content-container">
+            <ContentModal isOpen={!isOpen} />
             <Row className="content-box">
+                <Col className="content-element" lg={3} md={3} onClick={(event) => handleModal()}>
+                    <div className="body">
+                        <div className="header kanit-font ">
+                            Punisher: War Zone
+                        </div>
+                        <img src="https://image.tmdb.org/t/p/w1280/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" style={{ width: '100%' }} alt="bg" />
+                        <div className="footer baloo-font">
+                            Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
+                        </div>
+                    </div>
+                </Col>
+
                 <Col className="content-element" lg={3} md={3} onClick={(event) => handleModal(event)}>
-                    <ContentModal />
                     <div className="body">
                         <div className="header kanit-font ">
                             Punisher: War Zone
--- src/components/elements/contentModal/index.jsx
+++ src/components/elements/contentModal/index.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+import './styles.scss';
 
 const ContentModal = (props) => {
+
+    Modal.setAppElement('#root');
+    
+    const [isOpen, setIsOpen] = useState(true)
+
+    useEffect(() => {
+        setIsOpen(props.isOpen); 
+    }, [props.isOpen])
+
+    function handleClose() {
+        setIsOpen(false);
+    }
+
     return (
-        <>
-            MODAL
-        </>
+        <Modal isOpen={isOpen} onRequestClose={() => handleClose()}
+            aria={{
+                labelledby: "heading",
+                describedby: "full_description"
+            }}>
+            <h1 id="heading">Alert</h1>
+            <div id="full_description">
+                <p>Description goes here.</p>
+            </div>
+        </Modal>
     )
 };
 
