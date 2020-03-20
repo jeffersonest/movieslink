a170f7dd86cf301048147e0449106f46b3dfeb82
Jefferson Estevam <jesbmail@gmail.com>
2020-03-20 10:08:20 -0300

    created a modal layout

--- src/components/elements/contentList/index.jsx
+++ src/components/elements/contentList/index.jsx
@@ -13,8 +13,8 @@ const ContentList = () => {
     return (
         <div className="content-container">
             <ContentModal isOpen={isOpen}/>
-            <Row className="content-box" onClick={() => dispatch(openModal())}>
-                <Col className="content-element" lg={3}>
+            <Row className="content-box">
+                <Col className="content-element" lg={3} onClick={() => dispatch(openModal())}>
                     <div className="body">
                         <div className="header kanit-font ">
                             Punisher: War Zone
@@ -26,53 +26,7 @@ const ContentList = () => {
                     </div>
                 </Col>
 
-                <Col className="content-element" lg={3}>
-                    <div className="body">
-                        <div className="header kanit-font ">
-                            Punisher: War Zone
-                        </div>
-                        <img src="https://image.tmdb.org/t/p/w1280/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" style={{ width: '100%' }} alt="bg" />
-                        <div className="footer baloo-font">
-                            Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
-                        </div>
-                    </div>
-                </Col>
 
-                <Col className="content-element" lg={3}>
-                    <div className="body">
-                        <div className="header kanit-font ">
-                            Punisher: War Zone
-                        </div>
-                        <img src="https://image.tmdb.org/t/p/w1280/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" style={{ width: '100%' }} alt="bg" />
-                        <div className="footer baloo-font">
-                            Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
-                        </div>
-                    </div>
-                </Col>
-
-                <Col className="content-element" lg={3}>
-                    <div className="body">
-                        <div className="header kanit-font ">
-                            Punisher: War Zone
-                        </div>
-                        <img src="https://image.tmdb.org/t/p/w1280/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" style={{ width: '100%' }} alt="bg" />
-                        <div className="footer baloo-font">
-                            Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
-                        </div>
-                    </div>
-                </Col>
-
-                <Col className="content-element" lg={3}>
-                    <div className="body">
-                        <div className="header kanit-font ">
-                            Punisher: War Zone
-                        </div>
-                        <img src="https://image.tmdb.org/t/p/w1280/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" style={{ width: '100%' }} alt="bg" />
-                        <div className="footer baloo-font">
-                            Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
-                        </div>
-                    </div>
-                </Col>
             </Row>
         </div>
     )
--- src/components/elements/contentModal/index.jsx
+++ src/components/elements/contentModal/index.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import Modal from 'react-modal';
+import Modal from 'react-responsive-modal';
 import './styles.scss';
 import { closeModal } from '../../../actions/modal.action';
 import { useDispatch } from 'react-redux';
+import { Row, Col, Container } from 'react-grid-system';
 
 const ContentModal = (props) => {
 
-    Modal.setAppElement('#root');
     const dispatch = useDispatch();
 
     let isOpen = props.isOpen;
 
     return (
-        <Modal className="modal-box" isOpen={isOpen} onRequestClose={() => dispatch(closeModal())}
-            aria={{
-                labelledby: "heading",
-                describedby: "full_description"
-            }}>
-            <h1 id="heading">Alert</h1>
-            <div id="full_description">
-                <p>Description goes here.</p>
-            </div>
+        // (name, poster image, genre, overview,release date).
+        <Modal  className="modal-box" open={isOpen} onClose={() => dispatch(closeModal())}>
+            <Container >
+                <Row className="body">
+                    <Col lg={5} className="poster-box">
+                        <img className="poster-image" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" width="320" alt="poster"></img>
+                    </Col>
+                    <Col lg={7} className="poster-info">
+                        <Col lg={12} className="header">
+                            <span className="kanit-font">Punisher: War Zone <span className="year">(2020)</span></span>
+                            <p className="baloo-font">
+                                Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
+                            </p>
+                        </Col>
+                    </Col>
+                </Row>
+            </Container>
         </Modal>
     )
 };
