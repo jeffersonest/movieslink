02cba4ae0f187a63a3908f35387a6934a3d54665
Jefferson Estevam <jefferson.e.barbosa@accenture.com>
2020-03-20 20:30:24 -0300

    removed redux devtools, adjust close modal button

--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 
 const store = createStore(
-    Reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    Reducers
     );
 
 ReactDOM.render(
