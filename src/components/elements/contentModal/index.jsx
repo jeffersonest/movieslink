19723b26c7d15ff3a2cdc641e14da18a4ce540f9
Jefferson Estevam <jefferson.e.barbosa@accenture.com>
2020-03-20 20:25:35 -0300

    Edited Modal styles, Added genre TAG for movies etc...
     On branch development
     Changes to be committed:
    	modified:   src/components/elements/contentList/styles.scss
    	modified:   src/components/elements/contentModal/index.jsx
    	modified:   src/components/elements/contentModal/styles.scss
    	modified:   src/components/elements/paginate/index.jsx
    	modified:   src/components/elements/paginate/styles.scss

--- src/components/elements/contentModal/index.jsx
+++ src/components/elements/contentModal/index.jsx
@@ -14,18 +14,22 @@ const ContentModal = (props) => {
     return (
         // (name, poster image, genre, overview,release date).
         <Modal  className="modal-box" open={isOpen} onClose={() => dispatch(closeModal())}>
+            <span className="gender-tag anton-font">TERROR</span> 
             <Container >
                 <Row className="body">
+                     
                     <Col lg={5} className="poster-box">
                         <img className="poster-image" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/i6Zg52KFtG5vCZXBMMyoLLsQAHU.jpg" width="320" alt="poster"></img>
                     </Col>
-                    <Col lg={7} className="poster-info">
+                    <Col lg={7} className="movie-info">
                         <Col lg={12} className="header">
-                            <span className="kanit-font">Punisher: War Zone <span className="year">(2020)</span></span>
-                            <p className="baloo-font">
-                                Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... 
-                            </p>
-                        </Col>
+                            <span className="kanit-font">Punisher: War Zone <span className="year">(2020) </span></span>
+                        </Col>    
+                        <Col lg={12} className="movie-info-body">
+                            <span className="baloo-font">Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left... </span>
+                                                
+                        </Col> 
+                        
                     </Col>
                 </Row>
             </Container>
--- src/components/elements/paginate/index.jsx
+++ src/components/elements/paginate/index.jsx
@@ -7,9 +7,9 @@ const Pagination = (props) => {
             <div className="index-box">
                 <button className="index-button anton-font">Prev</button>
                     <div className="pages-container">
-                        <span className="page-number">1</span>
+                        {/* <span className="page-number">1</span>
                         <span className="page-number">2</span>
-                        <span className="page-number">3</span>
+                        <span className="page-number">3</span> */}
                     </div>
                 <button className="index-button anton-font">Next</button>
             </div>
